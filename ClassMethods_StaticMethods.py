a026f2e076b365a28e6b14146f76c68ac5b6cabc
cesardushime <125463453+cesardushime@users.noreply.github.com>
2024-11-08 13:58:46 +0100

    created and and used classmethod as a constructor alternative

--- ClassMethods_StaticMethods.py
+++ ClassMethods_StaticMethods.py
@@ -4,7 +4,7 @@ class Employee():
     raise_amount = 1.1
     
     def __init__(self, first, last, pay):
-        # data unique to each instance, created using self
+        # data unique to each instance, it's created using self
         self.first = first
         self.last = last
         self.pay = pay
@@ -19,14 +19,30 @@ class Employee():
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount) # Access class variable by either class or instance of the class (self)
         return self.pay
+    
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+        
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+    
+
+# Employee.set_raise_amount(1.05)
 
 emp_1 = Employee('Diego', 'Raynman', 50000)
 emp_2 = Employee('Sam', 'Akhma', 34000)
 
+emp_str1 = 'John-Doe-70000'
+emp_str2 = 'Steve-Smith-30000'
+emp_str3 = 'Jane-Doe-90000'
+
+# using classmethod as an alternative constructor 
+new_emp1 = Employee.from_string(emp_str1)
 
 
-print(f'{emp_1.fullName()} got a raise from {emp_1.pay} to {emp_1.apply_raise()}')
-print(f'{emp_2.fullName()} got a raise from {emp_2.pay} to {emp_2.apply_raise()}')
+print(new_emp1.fullName())
+print(new_emp1.pay)
 
-print("There are", Employee.num_of_emps, 'employees')
-print(f"Total Pays for {Employee.num_of_emps} employees is {(emp_1.apply_raise() + emp_2.apply_raise()): ,}")
