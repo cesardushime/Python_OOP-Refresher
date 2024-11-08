596f4d0399da52cbdf5148aee50234fb27641ed1
cesardushime <125463453+cesardushime@users.noreply.github.com>
2024-11-08 14:17:04 +0100

    created a static method with checking working day

--- ClassMethods_StaticMethods.py
+++ ClassMethods_StaticMethods.py
@@ -1,3 +1,4 @@
+
 class Employee():
     
     num_of_emps = 0
@@ -29,20 +30,30 @@ class Employee():
         first, last, pay = emp_str.split('-')
         return cls(first, last, pay)
     
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return 'Not a working day'
+        return "It's a working day"
+    
 
 # Employee.set_raise_amount(1.05)
 
 emp_1 = Employee('Diego', 'Raynman', 50000)
 emp_2 = Employee('Sam', 'Akhma', 34000)
 
-emp_str1 = 'John-Doe-70000'
-emp_str2 = 'Steve-Smith-30000'
-emp_str3 = 'Jane-Doe-90000'
+# emp_str1 = 'John-Doe-70000'
+# emp_str2 = 'Steve-Smith-30000'
+# emp_str3 = 'Jane-Doe-90000'
 
-# using classmethod as an alternative constructor 
-new_emp1 = Employee.from_string(emp_str1)
+# # using classmethod as an alternative constructor  
+# new_emp1 = Employee.from_string(emp_str1)
 
 
-print(new_emp1.fullName())
-print(new_emp1.pay)
+# print(new_emp1.fullName())
+# print(new_emp1.pay)
+
+import datetime
+my_date = datetime.date(2024,11,8)
+print(Employee.is_workday(my_date))
 
